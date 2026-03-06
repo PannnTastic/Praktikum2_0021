bfe93c553fec99a43b36a52d4421b2ce0bf89fab
PannnTastic <pannntastic@gmail.com>
2026-03-06 13:05:36 +0700

    update background color dan tambah body dengan Text "Praktikum2" di praktikum2.dart

--- lib/praktikum2.dart
+++ lib/praktikum2.dart
@@ -6,7 +6,7 @@ class Praktikum2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -22,8 +22,23 @@ class Praktikum2 extends StatelessWidget {
                 Icons.settings, color: Colors.black,
               ),
               onPressed: (){}
-          )
+          ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: [
+            const Text(
+              "Praktikum2",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
