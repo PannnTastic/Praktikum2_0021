45fe43dd9c13358fd7e28db369b70b61136f7ccc
PannnTastic <pannntastic@gmail.com>
2026-03-06 12:44:21 +0700

    Initial commit

---
+++ lib/praktikum2.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Praktikum2 extends StatelessWidget {
+  const Praktikum2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+
